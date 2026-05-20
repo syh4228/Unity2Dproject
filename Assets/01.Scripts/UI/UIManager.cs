5a7ba3f03f8c4100894909e8a5486302cc4d823a
신영준 <kdrsa789@gmail.com>
2026-05-20 18:03:40 +0900

    UI매니저 컴포넌트 수정 ( 메인 UI에서는 다른 UI를 활성화, 비활성화를 관리만 하도록 수정)

--- Assets/01.Scripts/UI/UIManager.cs
+++ Assets/01.Scripts/UI/UIManager.cs
@@ -6,16 +6,12 @@ public class UIManager : MonoBehaviour
     public static UIManager Instance { get; private set; } // 싱글턴 선언
 
     [Header("UI 컴포넌트")]
-    [SerializeField] private GameObject mainUI; // 메인 UI
-    [SerializeField] private GameObject gameOverCanvas; // 게임오버 UI
-    [SerializeField] private BattleUIManager battleUI; // 배틀UI
     [SerializeField] private GameObject startUI; //  스타트 UI
-    [SerializeField] private StartUIManager loadingManager; // 스타트 UI 로딩 연출
-
-    public BattleUIManager GetBattleUI() // 배틀UI 주는 함수
-    {
-        return battleUI; // 배틀UI 반환
-    }
+    [SerializeField] private GameObject mainUI; // 메인 UI
+    [SerializeField] private GameObject LobbyUI; // 로비 UI
+    [SerializeField] private GameObject LoadingUI; // 로딩 UI
+    [SerializeField] private GameObject battleUI; // 배틀UI
+    [SerializeField] private GameObject scoreUI; // 점수UI
 
     private void Awake()
     {
@@ -29,68 +25,97 @@ public class UIManager : MonoBehaviour
         }
     }
 
-    private void Start()
+    public void Allcolse() // UI 전체 비활성화 함수
     {
-        ResetUI(); // 시작시 UI 함수 호출
-    }
-
-    public void ResetUI() // 시작시 UI 리셋 함수
-    {
-        if (startUI != null) // 만약  스타트 UI 있으면
+        if (startUI  != null)
         {
-            startUI.SetActive(true); // 스타트 UI 활성화
-        }
-        if (mainUI != null) // 메인 UI가 있으면
-        {
-            mainUI.SetActive(false); // 비 활성화
-        }
-        if (gameOverCanvas != null) // 게임오버 UI가 있으면
-        {
-            gameOverCanvas.SetActive(false); // 비활성화
-        }
-    }
-
-    // 스타트 UI 호출 함수
-    public void showStartUI()
-    {
-        if (startUI != null) // 만약 스타트 UI가 있으면
-        {
-            startUI.SetActive(true); // 스타트 UI 활성화
+            startUI.SetActive(false);
         }
 
-        if (loadingManager != null) // 로딩 매니저가 있으면
+        if (mainUI != null)
         {
-            // 로딩매니저 스타트 로딩 함수 호출
-            loadingManager.StartLoading(OnLoadingComplete);
+            mainUI.SetActive(false);
+        }
+
+        if (LobbyUI != null)
+        {
+            LobbyUI.SetActive(false);
+        }
+
+        if (LoadingUI != null)
+        {
+            LoadingUI.SetActive(false);
+        }
+
+        if (battleUI != null)
+        {
+            battleUI.SetActive(false);
+        }
+
+        if (scoreUI != null)
+        {
+            scoreUI.SetActive(false);
         }
     }
 
-    // 스타트 UI 끝나면 메인 UI로 전환하는 함수
-    private void OnLoadingComplete()
+    public void CallStartUI() // 스타트 UI 호출 함수
     {
-        if (startUI != null) // 만약 스타트UI 가 있으면
-        {
-            startUI.SetActive(false); // 비활성화
-        }
+        Allcolse(); // 전체 한번 비활성화
 
-        if (mainUI != null) // 만약 메인 UI 가 있으면
+        if (startUI != null)
         {
-            mainUI.SetActive(true); // 메인 UI 활성화
+            startUI.SetActive(true);
         }
-
-        UtillLogRemove.Log("로딩 종료 -> 메인 UI 전환 완료 (정석 함수 실행됨)");
     }
 
-    // 게임오버 UI 호출 함수
-    public void ShowGameOver()
+    public void CallMainUI()
     {
-        if (mainUI != null) // 메인UI 있으면
+        Allcolse();
+       
+        if (mainUI != null)
         {
-            mainUI.SetActive(false); // 비활성화
-        }
-        if (gameOverCanvas != null) // 게임오버 UI 있으면
-        {
-            gameOverCanvas.SetActive(true); // 활성화
+            mainUI.SetActive(true);
         }
     }
+
+    public void CallLobbyUI()
+    {
+        Allcolse();
+
+        if (LobbyUI != null)
+        {
+            LobbyUI.SetActive(true);
+        }
+    }
+
+    public void CallLoadingUI()
+    {
+        Allcolse();
+
+        if (LoadingUI != null)
+        {
+            LoadingUI.SetActive(true);
+        }
+    }
+
+    public void CallBattleUI()
+    {
+        Allcolse();
+
+        if (battleUI != null)
+        {
+            battleUI.SetActive(true);
+        }
+    }
+
+    public void CallscoreUI()
+    {
+        Allcolse();
+
+        if (scoreUI != null)
+        {
+            scoreUI.SetActive(true);
+        }
+    }
+
 }
