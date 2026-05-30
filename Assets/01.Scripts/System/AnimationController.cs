c1e3321c74f3513c13b428bb72034efb0c77b42c
신영준 <kdrsa789@gmail.com>
2026-05-30 19:08:12 +0900

    애니메이션 컨트롤러 컴포넌트 수정 (딕셔너리에서 -> 스위치로) 및 애니메이션들 추가

--- Assets/01.Scripts/System/AnimationController.cs
+++ Assets/01.Scripts/System/AnimationController.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using static UnityEngine.Rendering.DebugUI;
 
-public enum AllState // 플레이어 상태
+public enum AllState // 공유할 상태 목록
 {
     Idle,
     Walk,
     Run,
     Attack,
-    Dead
+    Dead,
+    Hit,
+    Jump,
+    UseHeal, // 힐킷
+    UseInstantHeal, // 구급얍, 아드레날린
+    UseGrenade // 슈륙탄
 }
 
 // 애니매이션 전체 조작을 담당하는 컨트롤러
@@ -17,30 +22,21 @@ public class AnimationController : MonoBehaviour
 {
     [SerializeField] private Animator Animator_Control; // 애니메이터 연결
 
-    private AllState _currentState; // 현재 상태
-
-    private Dictionary<AllState, Action> _animationActions; // 딕셔너리로 모든 애니메이션 행동 저장
+    private AllState _currentState; // 현재 상태 저장
 
     // 애니매이션 상태를 해시 숫자로 저장(한번 선언 시 변환 불가)
-    private static readonly int AnimationIsRun = Animator.StringToHash("IsRun");
-    private static readonly int AnimationIsDead = Animator.StringToHash("IsDead");
-    private static readonly int AnimationIsAttack = Animator.StringToHash("IsAttack");
+    // bool 스위치 (재생이 유지 되야하는 경우)
     private static readonly int AnimationIsWalk = Animator.StringToHash("IsWalk");
+    private static readonly int AnimationIsRun = Animator.StringToHash("IsRun");
     private static readonly int AnimationIsGrounded = Animator.StringToHash("isGrounded");
-    private static readonly int AnimationIsHit = Animator.StringToHash("IsHit");
+    private static readonly int AnimationIsDead = Animator.StringToHash("IsDead");
+    private static readonly int AnimationIsHealing = Animator.StringToHash("IsHealing");
 
-    private void Awake()
-    {
-        // 상태에 맞는 행동을 딕셔너리로 저장
-        _animationActions = new Dictionary<AllState, Action>();
-
-        // 각 상태에 따른 애니메이션 실행 함수 연결
-        _animationActions.Add(AllState.Idle, AllIdleAnimation);
-        _animationActions.Add(AllState.Walk, AllWalkAnimation);
-        _animationActions.Add(AllState.Run, AllRunAnimation);
-        _animationActions.Add(AllState.Dead, AllDeadAnimation);
-        _animationActions.Add(AllState.Attack, AllAttackAnimation);
-    }
+    // Trigger 스위치 (한번씩만 재생하는 경우)
+    private static readonly int AnimationIsAttack = Animator.StringToHash("IsAttack");
+    private static readonly int AnimationTriggerHit = Animator.StringToHash("IsHit");
+    private static readonly int AnimationTriggerGrenade = Animator.StringToHash("Grenade");
+    private static readonly int AnimationTriggerInstantHeal = Animator.StringToHash("InstantHeal");
 
     private void Start()
     {
@@ -65,68 +61,69 @@ public class AnimationController : MonoBehaviour
         // 만약 바뀌는 상태와 현재상태가 같으면
         if (newState == _currentState)
         {
-            // 만약  현재상태가 공격이 아니면
-            if (newState != AllState.Attack)
+            // 만약  현재상태가 공격, 피격, 슈륙탄 투척, 힐킷사용이 아니면( 연속 재생 가능 경우 애니메이션)
+            if (newState != AllState.Attack && newState != AllState.Hit && newState != AllState.UseGrenade && newState != AllState.UseInstantHeal) 
             {
                 return; // 반환
             }
         }
 
-        // 만약 현재상태가 딕셔너리에 있다면
-        if (_animationActions.TryGetValue(newState, out Action action))
+        // 기존 스위치들 모두 끄기
+        ResetAllBoolParameters();
+
+        switch (newState)
         {
-            // 액션 실행
-            action.Invoke();
-            // 현재상태를 다음상태로 저장
-            _currentState = newState;
+            case AllState.Idle:
+                break;
+            case AllState.Walk:
+                SafeSetBool(AnimationIsWalk, true);
+                break;
+            case AllState.Run:
+                SafeSetBool(AnimationIsRun, true);
+                break;
+            case AllState.Dead:
+                SafeSetBool(AnimationIsDead, true);
+                break;
+            case AllState.Attack:
+                SafeSetTrigger(AnimationIsAttack);
+                break;
+            case AllState.Hit:
+                SafeSetTrigger(AnimationTriggerHit);
+                break;
+            case AllState.UseHeal:
+                SafeSetBool(AnimationIsHealing, true);
+                break;
+            case AllState.UseInstantHeal:
+                SafeSetTrigger(AnimationTriggerInstantHeal);
+                break;
+            case AllState.UseGrenade:
+                SafeSetTrigger(AnimationTriggerGrenade);
+                break;
+            default:
+                UtillLogRemove.Warning($"{newState} 상태에 대한 처리가 switch문에 없습니다.");
+                break;
         }
-        else
-        {
-            // 딕셔너리에 저장안된 함수 호출시 로그 호출
-            UtillLogRemove.Warning($"{newState} 연결된 애니메이션이 없습니다.");
-        }
+
+        _currentState = newState; // 바뀐 상태 저장
     }
 
-    public void SetGrounded(bool isGrounded) // 점프 애니메이션 함수
+    // 점프 애니메이션 함수 (점프 중에도 다른 동작으로 변환 할 수 있도록 따로 호출)
+    public void SetGrounded(bool isGrounded)
     {
         if (Animator_Control != null)
         {
+            // isGrounded를 스스로 끄거나 킴
             SafeSetBool(AnimationIsGrounded, isGrounded);
         }
     }
 
-    private void AllIdleAnimation() // 대기애니메이션 함수
+    // 힐킷 애니메이션 함수 (힐킷 사용중에도 다른 동작으로 변환 할 수 있도록 따로 호출
+    public void SetHealing(bool isHealing) 
     {
-        // 상태 초기화 함수 호출
-        ResetAllBoolParameters();
-    }
-
-    private void AllWalkAnimation() // 걷는 애니매이션 함수
-    {
-        ResetAllBoolParameters();
-        // 걷기 애니메이션 실행
-        SafeSetBool(AnimationIsWalk, true);
-    }
-
-    private void AllRunAnimation() // 달리기 애니메이션 함수
-    {
-        ResetAllBoolParameters();
-        // 달리기 애니메이션 실행
-        SafeSetBool(AnimationIsRun, true);
-    }
-
-    private void AllDeadAnimation() // 죽음 애니메이션 함수
-    {
-        ResetAllBoolParameters();
-        // 죽음 애니메이션 실행
-        SafeSetBool(AnimationIsDead, true);
-    }
-
-    private void AllAttackAnimation() // 공격 애니메이션 함수
-    {
-        ResetAllBoolParameters();
-        // 공격 애니메이션 실행
-        SafeSetBool(AnimationIsAttack, true);
+        if (Animator_Control != null)
+        {
+            SafeSetBool(AnimationIsHealing, isHealing);
+        }
     }
 
     // 상태 초기화 함수
@@ -136,13 +133,11 @@ public class AnimationController : MonoBehaviour
         SafeSetBool(AnimationIsWalk, false);
         // 달리기 애니메이션 끄기
         SafeSetBool(AnimationIsRun, false);
-        // 죽음 애니메이션 끄기
-        SafeSetBool(AnimationIsDead, false);
-        // 공격 애니메이션 끄기
-        SafeSetBool(AnimationIsAttack, false);
+        // 힐킷 애니메이션 끄기
+        SafeSetBool(AnimationIsHealing, false);
     }
 
-    // 애니메이션 파라미터 조정 함수
+    // 애니메이션 스위치 조작 함수
     private void SafeSetBool(int parameterHash, bool value)
     {
         if (Animator_Control == null) // 애니메이션 컨트롤러가 없으면
@@ -151,40 +146,35 @@ public class AnimationController : MonoBehaviour
         }
 
         // 애니메이터가 가진 파라미터 목록을 하니씩 검사
-        foreach (AnimatorControllerParameter param in Animator_Control.parameters)
+        foreach (AnimatorControllerParameter animatorParam in Animator_Control.parameters)
         {
-            // 해시 번호 바꾸려는 번호와 맞는지 체크
-            if (param.nameHash == parameterHash)
+            // 해시 번호로 파라미터가 있는지 확인
+            if (animatorParam.nameHash == parameterHash)
             {
                 // 스위치가 실제로 있을때만 값 변경
                 Animator_Control.SetBool(parameterHash, value);
+
                 return; // 반환
             }
         }
     }
 
-    public void AllHitAnimation() // 피격 애니메이션 함수
-    {
-        ResetAllBoolParameters();
-        // 피격 애니메이션 실행
-        SafeSetTrigger(AnimationIsHit);
-    }
-
-    // 파라미터 트리거 함수
+    // 애니메이션 트리거 조작 함수
     private void SafeSetTrigger(int parameterHash)
     {
         if (Animator_Control == null) // 널 체크
         {
             return;
-        }
+        } 
 
-        foreach (AnimatorControllerParameter param in Animator_Control.parameters)
+        foreach (AnimatorControllerParameter animatorParam in Animator_Control.parameters)
         {
-            if (param.nameHash == parameterHash)
+            if (animatorParam.nameHash == parameterHash)
             {
-                // 트리거 작동
+                // 트리거 발동
                 Animator_Control.SetTrigger(parameterHash);
-                return; // 반환
+
+                return;
             }
         }
     }
