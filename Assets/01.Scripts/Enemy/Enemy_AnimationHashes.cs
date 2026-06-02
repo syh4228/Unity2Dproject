9b20f9f6284ab3466d4cd2fbbfd8236364471c51
신영준 <kdrsa789@gmail.com>
2026-06-02 22:30:04 +0900

    적 공통 애니메이션 부분 파라미터 해시 변환용 컴포넌트 제작

---
+++ Assets/01.Scripts/Enemy/Enemy_AnimationHashes.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+// 적 애니메이션 공통 파라미터 해시 변환용 컴포넌트
+public class Enemy_AnimationHashes : MonoBehaviour
+{
+    public static readonly int Idle = Animator.StringToHash("IsIdle");
+    public static readonly int Hit = Animator.StringToHash("IsHit");
+    public static readonly int Run = Animator.StringToHash("IsRun");
+    public static readonly int Attack = Animator.StringToHash("IsAttack");
+    public static readonly int Die = Animator.StringToHash("IsDead");
+}
